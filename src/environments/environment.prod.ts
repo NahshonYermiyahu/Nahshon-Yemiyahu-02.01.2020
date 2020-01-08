839a735b57d2dfac2f9c1cdcc71b80f62a6f1e87
Nahshon <nahshonrm@gmail.com>
2020-01-08 12:43:04 +0200

    8.01/7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyB7PFAJdV0UaRy6nGNRx9ze-Zj7qzJoOWg",
+      authDomain: "weatherrm-92147.firebaseapp.com",
+      databaseURL: "https://weatherrm-92147.firebaseio.com",
+      projectId: "weatherrm-92147",
+      storageBucket: "weatherrm-92147.appspot.com",
+      messagingSenderId: "574890269636",
+      appId: "1:574890269636:web:c3e505ff01c1c3f274e291",
+      measurementId: "G-78EYRLWYNL"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB7PFAJdV0UaRy6nGNRx9ze-Zj7qzJoOWg",
+    authDomain: "weatherrm-92147.firebaseapp.com",
+    databaseURL: "https://weatherrm-92147.firebaseio.com",
+    projectId: "weatherrm-92147",
+    storageBucket: "weatherrm-92147.appspot.com",
+    messagingSenderId: "574890269636",
+    appId: "1:574890269636:web:c3e505ff01c1c3f274e291",
+    measurementId: "G-78EYRLWYNL"
+  }
 };
 
 /*
